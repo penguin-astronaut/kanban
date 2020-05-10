e13dbf46b9a0514c2090aaddd31217623b1ea78e
Vadim Kasatkin <chack.k@mail.ru>
2020-05-10 22:38:47 +0400

    fix "link" for finished card

--- src/components/kanban-components/cards.js
+++ src/components/kanban-components/cards.js
@@ -3,7 +3,7 @@ import withSelect from "../hoc-helpers/withSelect";
 
 const ReadyCard = withSelect(Card, "Ready", "ready");
 const InProgressCard = withSelect(Card, "In Progress", "in_progress");
-const FinishedCard = withSelect(Card, "Finished", "finishes");
+const FinishedCard = withSelect(Card, "Finished", "finished");
 
 export {
   ReadyCard,
